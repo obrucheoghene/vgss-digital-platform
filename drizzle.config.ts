99f98cad6762d7c0f331db97606a5e5636780882
obrucheoghene <obrucheoghene1@gmail.com>
2025-08-24 20:05:48 +0100

    add drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+// drizzle.config.ts
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./src/lib/db/schema.ts",
+  out: "./src/lib/db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+} satisfies Config;
